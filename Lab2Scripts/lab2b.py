ce64d6e4dbc0450fd71ea82dfa29ee0d9c335a1d
epatel16-bit <61957406+epatel16-bit@users.noreply.github.com>
2025-01-22 18:04:59 -0500

    Delete Lab2Scripts directory

--- Lab2Scripts/lab2b.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Prompt the user for their name and age
-name = input("Name: ")
-age = int(input("Age: "))  # Convert the age input to an integer
-
-# Print the exact output as required
-print("Hi " + name + ", you are " + str(age) + " years old.")  # Convert age back to string for printing
-
